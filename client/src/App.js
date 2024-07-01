2f8ebbaf7364d098e9b1af111cf4cc8fa0e94f0c
Makarona10 <ahmedneyssi@gmail.com>
2024-07-01 03:28:42 +0300

    Browsing page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Nav_bar } from './components/Navbar/Navbar.js';
-import { BrandBar } from './components/brandBar/brandBar.js'
-import { Categoside } from './components/categories/categories.js';
-import { ListProd } from './components/container/container.js';
+import { BrowseProd } from './components/browseProd/browseProd';
 
 function App() {
   return (
     <div className="App">
-      <BrandBar />
-      <Nav_bar />
-      <Categoside />
-      <ListProd />
+      <BrowseProd />
     </div>
   );
 }
---
+++ client/src/components/browseProd/browseProd.js
@@ -0,0 +1,15 @@
+import { BrandBar } from '../brandBar/brandBar.js';
+import { Nav_bar } from '../Navbar/Navbar.js';
+import { ListProd } from '../container/container.js';
+import { Categoside } from '../categories/categories.js';
+
+export const BrowseProd = () => {
+    return (
+        <div>
+            <BrandBar />
+            <Nav_bar />
+            <Categoside />
+            <ListProd />
+        </div>
+    )
+}
