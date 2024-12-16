6202410a4dff22ced6c619c4dd0f74e2562c333c
Ahmed Abdelnaby <120122678+Makarona10@users.noreply.github.com>
2024-12-16 07:58:34 +0200

    Delete server/models directory (Trash)

--- server/models/merchants.js
+++
@@ -1,9 +0,0 @@
-import { connection } from '../DB/index.js';
-
-const merchants_table = `CREATE TABLE IF NOT EXISTS merchants (
-    id BIGINT REFERENCES users(id),
-    store_name VARCHAR(255),
-    location VARCHAR(255)
-    )`;
-
-connection.query(merchants_table);
